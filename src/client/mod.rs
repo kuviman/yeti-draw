ba7bf7f4d4041f0ee25741075c0d45581d346b0a
kuviman <kuviman@gmail.com>
2022-05-23 20:29:57 +0400

    Drawing strokes

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -8,6 +8,12 @@ pub struct Client {
     state: State,
     framebuffer_size: Vec2<usize>,
     camera: geng::Camera2d,
+    stroke: Option<Stroke>,
+}
+
+struct Stroke {
+    pixels: HashSet<Vec2<i32>>,
+    last_position: Vec2<f32>,
 }
 
 impl Client {
@@ -22,6 +28,7 @@ impl Client {
                 rotation: 0.0,
                 fov: 100.0,
             },
+            stroke: None,
         }
     }
 }
@@ -50,6 +57,18 @@ impl geng::State for Client {
                 ),
             );
         }
+        if let Some(stroke) = &self.stroke {
+            for &position in &stroke.pixels {
+                self.geng.draw_2d(
+                    framebuffer,
+                    &self.camera,
+                    &draw_2d::Quad::new(
+                        AABB::point(position.map(|x| x as f32)).extend_positive(vec2(1.0, 1.0)),
+                        Color::BLACK,
+                    ),
+                );
+            }
+        }
     }
     fn handle_event(&mut self, event: geng::Event) {
         match event {
@@ -61,11 +80,48 @@ impl geng::State for Client {
                     self.framebuffer_size.map(|x| x as f32),
                     position.map(|x| x as f32),
                 );
-                self.connection
-                    .send(ClientMessage::Update(Update::Draw(vec![Pixel {
-                        position: position.map(|x| x.floor() as i32),
-                        color: Color::BLACK,
-                    }])));
+                self.stroke = Some(Stroke {
+                    pixels: default(),
+                    last_position: position,
+                });
+            }
+            geng::Event::MouseMove { position, delta: _ } => {
+                let position = self.camera.screen_to_world(
+                    self.framebuffer_size.map(|x| x as f32),
+                    position.map(|x| x as f32),
+                );
+                if let Some(stroke) = &mut self.stroke {
+                    let v = position - stroke.last_position;
+                    let n = (v.len() * 2.0 + 1.0) as i32;
+                    for i in 0..=n {
+                        stroke.pixels.insert(
+                            (stroke.last_position + v * i as f32 / n as f32)
+                                .map(|x| x.floor() as i32),
+                        );
+                    }
+                    stroke.last_position = position;
+                }
+            }
+            geng::Event::MouseUp {
+                position,
+                button: geng::MouseButton::Left,
+            } => {
+                let position = self.camera.screen_to_world(
+                    self.framebuffer_size.map(|x| x as f32),
+                    position.map(|x| x as f32),
+                );
+                if let Some(stroke) = self.stroke.take() {
+                    self.connection.send(ClientMessage::Update(Update::Draw(
+                        stroke
+                            .pixels
+                            .into_iter()
+                            .map(|position| Pixel {
+                                position,
+                                color: Color::BLACK,
+                            })
+                            .collect(),
+                    )));
+                }
             }
             _ => {}
         }
